54878bff8843fbe29b2b57b6ca71a1696dd85187
tanthanh2901 <nguyentanthanh29012003@gmail.com>
2024-08-29 11:57:45 +0700

    sql

--- QuanLyNhaSach/Form1.cs
+++ QuanLyNhaSach/Form1.cs
@@ -16,5 +16,10 @@ namespace QuanLyNhaSach
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
