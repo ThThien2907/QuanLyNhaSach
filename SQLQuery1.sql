613d247e176a1c8c1936b9048122e249902b5c22
tanthanh2901 <nguyentanthanh29012003@gmail.com>
2024-08-29 11:34:58 +0700

    sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,20 +10,18 @@ create table HOADON
 	MAHOADON char(7) primary key not null,
 	TENKHACHHANG nvarchar(50),
 	NGAYLAP datetime,
-	MASACH char(7),
-	SOLUONG int,
-	GIABAN int,
-	THANHTIEN AS (SOLUONG * GIABAN),
 	TONGTIEN decimal(10, 2) 
 )
-go
 
-create table KHO
+create table CHITIETHOADON
 (
-	MASACH char(7) primary key not null,
-	SOLUONG int
+	MAHOADON char(7) not null,
+	MASACH char(7),
+	SOLUONG int,
+	GIATIEN int,
+	THANHTIEN int
 )
-go
+
 
 create table LOAISACH
 (
@@ -42,9 +40,9 @@ go
 create table SACH
 (
 	MASACH char(7) primary key not null,
+	SOLUONG int,
 	TENSACH nvarchar(100),
 	MATG char(7) not null,
-	TENLINHVUC nvarchar(30),
 	MALOAISACH char(7),
 	GIABAN int,
 	GIANHAP int,
@@ -81,6 +79,8 @@ GO
 
 
 --Thêm khóa ngoại
+alter table CHITIETHOADON add constraint FK_CHITIETHOADON_HOADON foreign key(MAHOADON) references HOADON(MAHOADON)
+alter table CHITIETHOADON add constraint FK_CHITIETHOADON_SACH foreign key(MASACH) references SACH(MASACH)
 alter table HOADON add constraint FK_CHITIETHOADON_SACH foreign key(MASACH) references SACH(MASACH)
 alter table KHO add constraint FK_KHO_SACH foreign key(MASACH) references SACH(MASACH)
 alter table SACH add constraint FK_SACH_LOAISACH foreign key(MALOAISACH) references LOAISACH(MALOAISACH)
@@ -111,19 +111,12 @@ VALUES
 ('LS004', N'Công nghệ'),
 ('LS005', N'Nghệ thuật');
 
-INSERT INTO SACH (MASACH, TENSACH, MATG, MALOAISACH, GIABAN, GIANHAP, LANTAIBAN, MANXB, NAMXUATBAN)
+INSERT INTO SACH (MASACH, SOLUONG, TENSACH, MATG, MALOAISACH, GIABAN, GIANHAP, LANTAIBAN, MANXB, NAMXUATBAN)
 VALUES
-('S001', 'Harry Potter', 'TG002', 'LS001', 200000, 150000, 5, 'NXB002', 1997),
-('S002', '1984', 'TG003', 'LS003', 180000, 140000, 3, 'NXB003', 1949),
-('S003', 'Kafka on the Shore', 'TG004', 'LS001', 220000, 180000, 2, 'NXB001', 2002),
-('S004', 'Sapiens', 'TG005', 'LS003', 250000, 200000, 4, 'NXB005', 2011),
-('S005', 'Mat Biec', 'TG001', 'LS001', 150000, 120000, 6, 'NXB004', 1990);
+('S001', 10, 'Harry Potter', 'TG002', 'LS001', 200000, 150000, 5, 'NXB002', 1997),
+('S002', 10, '1984', 'TG003', 'LS003', 180000, 140000, 3, 'NXB003', 1949),
+('S003', 10, 'Kafka on the Shore', 'TG004', 'LS001', 220000, 180000, 2, 'NXB001', 2002),
+('S004',  10,'Sapiens', 'TG005', 'LS003', 250000, 200000, 4, 'NXB005', 2011),
+('S005', 10, 'Mat Biec', 'TG001', 'LS001', 150000, 120000, 6, 'NXB004', 1990);
 
 
-INSERT INTO KHO (MASACH, SOLUONG)
-VALUES
-('S001', 100),
-('S002', 80),
-('S003', 150),
-('S004', 120),
-('S005', 200);
